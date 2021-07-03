82257a7b82971f5bd463c6a768feeb6d13fbc819
Sophie Stupalo <55926599+sstupalo@users.noreply.github.com>
2021-07-02 22:10:18 -0400

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,20 @@
+int SToe     = A0;
+int SLateral = A1;
+int SBall    = A2;
+int SHeel    = A3;
+int MFront   = 5;
+int MLateral = 6;
+int MMedial  = 10;
+int MBack    = 11;
+
+
+
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
